27a416726de32cedf25736784d92c13c31a4e248
Serhii Shvets <wert2all@gmail.com>
2024-02-05 17:39:47 +0200

    fix color scheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "gruvbox-baby",
+  colorscheme = "gruvbox",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
