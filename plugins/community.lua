cf51f1c600631dabc507db11a857f9081ad3180f
Serhii Shvets <wert2all@gmail.com>
2024-02-05 02:52:56 +0200

    add php support

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.angular"},
+  { import = "astrocommunity.pack.php"},
   { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
